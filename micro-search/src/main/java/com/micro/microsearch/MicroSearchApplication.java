369f60a516138c47526d636349eea8b7077cfe3c
zhouxinglong <zhouxinglong@ekuaibao.com>
2019-09-23 17:30:03 +0800

    增加查询模块

---
+++ micro-search/src/main/java/com/micro/microsearch/MicroSearchApplication.java
@@ -0,0 +1,13 @@
+package com.micro.microsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroSearchApplication.class, args);
+	}
+
+}

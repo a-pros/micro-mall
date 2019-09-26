6d219653d436d470ee3cf6a4f2caf10ad7f1b36d
zhouxinglong <zhouxinglong@ekuaibao.com>
2019-09-26 10:35:56 +0800

    增加支付模块

---
+++ micro-pay/src/main/java/com/micro/pay/MicroPayApplication.java
@@ -0,0 +1,13 @@
+package com.micro.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroPayApplication.class, args);
+    }
+
+}
